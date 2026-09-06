9faed2d489131281ff43967f780725bb5c9b3b47
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-06 16:20:27 -0300

    chore: atualiza identificacao do firmware

--- firmware/core/jr_config.h
+++ firmware/core/jr_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JR_APP_VERSION "JRBotV2_2026-09-06-09:37"
-#define JR_BUILD_STAMP_SP "2026-09-06_09:37_BRT"
+#define JR_APP_VERSION "JRBotV2_2026-09-06-16:12"
+#define JR_BUILD_STAMP_SP "2026-09-06_16:12_BRT"
 
 #if __has_include("wifi_config.local.h")
 #include "wifi_config.local.h"
