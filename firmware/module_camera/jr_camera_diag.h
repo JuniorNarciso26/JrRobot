1d6f0b39a02eea99f680885e599a688caaeb5ce3
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-06 17:55:41 -0300

    feat: permite captura JPEG da camera pelo Wi-Fi

--- firmware/module_camera/jr_camera_diag.h
+++ firmware/module_camera/jr_camera_diag.h
@@ -1,6 +1,19 @@
 #pragma once
 #include <stdbool.h>
 #include <stddef.h>
-/* Serialized by the command router; never called from the boot sequence. */
+#include <stdint.h>
+#include "esp_err.h"
+
+/* Serialized by the camera module; never called from the boot sequence. */
+typedef struct {
+    uint8_t *data;
+    size_t len;
+    unsigned pid;
+    unsigned width;
+    unsigned height;
+} jr_camera_jpeg_t;
+
 bool jr_camera_probe_once(unsigned *pid_out);
 bool jr_camera_test_once(char *response, size_t capacity);
+esp_err_t jr_camera_capture_jpeg(jr_camera_jpeg_t *out);
+void jr_camera_jpeg_release(jr_camera_jpeg_t *frame);
