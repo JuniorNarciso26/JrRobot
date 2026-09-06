3dc4be4c793d45a4362749d431ab1cdfe043ea46
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-06 18:29:57 -0300

    chore: atualiza instalador dual serial

--- INSTALAR.bat
+++ INSTALAR.bat
@@ -102,7 +102,7 @@ exit /b %errorlevel%
 :success
 echo.
 echo OK - JrBot V2 atualizado.
-echo Firmware: JRBotV2_2026-09-06-17:54
+echo Firmware: JRBotV2_2026-09-06-18:27
 if defined JR_FLASH_PORT echo Hardware: JRBOT-HW-04 ^| Gravacao: %JR_FLASH_PORT%
 exit /b 0
 
