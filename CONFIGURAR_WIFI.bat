20e5cf050c4a7a70a04b3f4725b161dccabd9eb1
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-06 06:34:32 -0300

    chore: remove atalho de Wi-Fi redundante

--- CONFIGURAR_WIFI.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo No painel: Serial USB, COM4, Conectar, Configurar Wi-Fi.
-echo A senha nao deve ser escrita em arquivos de codigo ou publicada no GitHub.
-call "%~dp0PAINEL.bat"
-exit /b %errorlevel%
