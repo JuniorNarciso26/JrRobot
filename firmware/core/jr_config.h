2bde92ac2488272c3b5f1c18dec72a88dc5df284
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-06 18:38:29 -0300

    chore: atualiza versao para correcao USB dual

--- firmware/core/jr_config.h
+++ firmware/core/jr_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JR_APP_VERSION "JRBotV2_2026-09-06-18:27"
-#define JR_BUILD_STAMP_SP "2026-09-06_18:27_BRT"
+#define JR_APP_VERSION "JRBotV2_2026-09-06-18:28"
+#define JR_BUILD_STAMP_SP "2026-09-06_18:28_BRT"
 
 #if __has_include("wifi_config.local.h")
 #include "wifi_config.local.h"
