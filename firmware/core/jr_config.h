4855e2021e94b8ca3383fb159d1eab34d8cc0d49
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-09 06:07:00 -0300

    chore: identifica candidata Runtime API V1_05

--- firmware/core/jr_config.h
+++ firmware/core/jr_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JR_APP_VERSION "JRBotV2_RUNTIME_API_V1_04"
-#define JR_BUILD_STAMP_SP "2026-09-09_RUNTIME_API_V1_04"
+#define JR_APP_VERSION "JRBotV2_RUNTIME_API_V1_05"
+#define JR_BUILD_STAMP_SP "2026-09-09_RUNTIME_API_V1_05"
 
 #if __has_include("wifi_config.local.h")
 #include "wifi_config.local.h"
