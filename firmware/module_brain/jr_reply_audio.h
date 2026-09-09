8418586ae302b0d048a3f1aa71c5eecfa37303db
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-09 06:00:37 -0300

    perf: prepara resposta Oi em cache no boot

--- firmware/module_brain/jr_reply_audio.h
+++ firmware/module_brain/jr_reply_audio.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <stdbool.h>
 #include "esp_err.h"
 
 // Resposta local curta gerada no proprio ESP32, sem nuvem/TTS externo.
+void jr_reply_prepare_oi_async(void);
+bool jr_reply_oi_ready(void);
 esp_err_t jr_reply_play_oi(void);
