758e0fb5d8d228593759970408c744931b7371b4
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-14 11:41:57 -0300

    test(https): atualizar identificacao para EXP 03

--- firmware/core/jr_config.h
+++ firmware/core/jr_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JR_APP_VERSION "JrBot_HTTPS_EXP_02"
-#define JR_BUILD_STAMP_SP "2026-09-14_JrBot_HTTPS_EXP_02"
+#define JR_APP_VERSION "JrBot_HTTPS_EXP_03"
+#define JR_BUILD_STAMP_SP "2026-09-14_JrBot_HTTPS_EXP_03"
 
 #if __has_include("wifi_config.local.h")
 #include "wifi_config.local.h"
