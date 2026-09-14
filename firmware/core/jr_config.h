66ec0952a42bfe9757c48cf69d2a5c615f34d8d5
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-14 08:38:29 -0300

    fix(v1): identifica firmware como JrBot_V1.4.2

--- firmware/core/jr_config.h
+++ firmware/core/jr_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JR_APP_VERSION "JrBot_V1.4.1"
-#define JR_BUILD_STAMP_SP "2026-09-13_JrBot_V1.4.1"
+#define JR_APP_VERSION "JrBot_V1.4.2"
+#define JR_BUILD_STAMP_SP "2026-09-14_JrBot_V1.4.2"
 
 #if __has_include("wifi_config.local.h")
 #include "wifi_config.local.h"
