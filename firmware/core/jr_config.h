7d98785e93f05e1ecd47c68f6ce05dc71f454674
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-19 16:53:42 -0300

    chore: identifica ajuste de camera como JrBot_V1.6.3.3

--- firmware/core/jr_config.h
+++ firmware/core/jr_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JR_APP_VERSION "JrBot_V1.6.3.2"
-#define JR_BUILD_STAMP_SP "2026-09-19_JrBot_V1.6.3.2"
+#define JR_APP_VERSION "JrBot_V1.6.3.3"
+#define JR_BUILD_STAMP_SP "2026-09-19_JrBot_V1.6.3.3"
 
 #if __has_include("wifi_config.local.h")
 #include "wifi_config.local.h"
