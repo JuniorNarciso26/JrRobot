0bb451fa253fcca1ec5d572e5aff619e8b346d09
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-19 21:37:16 -0300

    feat: expoe endpoint WebSocket do microfone Live

--- firmware/module_audio/jr_mic.h
+++ firmware/module_audio/jr_mic.h
@@ -31,6 +31,7 @@ bool jr_mic_status(char *response, size_t capacity);
 esp_err_t jr_mic_record_wav_handler(httpd_req_t *req);
 esp_err_t jr_mic_live_pcm_handler(httpd_req_t *req);
 esp_err_t jr_mic_stream_handler(httpd_req_t *req);
+esp_err_t jr_mic_ws_handler(httpd_req_t *req);
 esp_err_t jr_mic_live_diag_handler(httpd_req_t *req);
 esp_err_t jr_mic_play_last_recording(void);
 void jr_mic_get_recording_info(jr_mic_recording_info_t *out);
