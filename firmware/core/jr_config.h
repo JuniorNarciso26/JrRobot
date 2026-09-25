767652a21a56892bc601c231ea7e6ac552764df6
JuniorNarciso26 <junior@cpgas.com.br>
2026-09-25 13:05:08 -0300

    chore: identifica build JrBot_V1.7_V2_02

--- firmware/core/jr_config.h
+++ firmware/core/jr_config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define JR_APP_VERSION "JrBot_V1.7_V2_01.1"
-#define JR_BUILD_STAMP_SP "2026-09-25_JrBot_V1.7_V2_01.1"
+#define JR_APP_VERSION "JrBot_V1.7_V2_02"
+#define JR_BUILD_STAMP_SP "2026-09-25_JrBot_V1.7_V2_02"
 
 #if __has_include("wifi_config.local.h")
 #include "wifi_config.local.h"
